1fd71cd7ccc1a4c5a7e3db35f4970503e8d3de42
0pen-Standards <contact@nouhtrang.com>
2025-10-28 08:41:03 -0500

    ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,15 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://nim-fawn.vercel.app/'),
+  metadataBase: new URL('https://nouhtrang.com'),
   alternates: {
     canonical: '/'
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Nouhtrang Thao — Designer',
+    template: '%s | Nouhtrang Thao'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description:  'Nouhtrang Thao — Designer',
 };
 
 const geist = Geist({
