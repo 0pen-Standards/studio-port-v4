cb5ec5b28bc161b9c7419d29dcc31aafafaa0f90
0pen-Standards <contact@nouhtrang.com>
2026-01-16 16:53:42 -0600

    Initial commit

--- app/selected-projects-2026/page.tsx
+++ app/selected-projects-2026/page.tsx
@@ -388,7 +388,8 @@ export default function SelectedProjects() {
               alt="g league give and go"
               width={1200}
               height={800}
-              className="h-auto w-full object-cover"
+              className="h-auto w-full object-cover
+              mt-2 pt-5 "
             />
             <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
               G League, Campaign Branding
@@ -418,12 +419,20 @@ export default function SelectedProjects() {
               alt="sapporo east meets west"
               width={1200}
               height={800}
+              className="h-auto w-full object-cover mt-2 pb-5 "
+            />
+                   <Image
+              src="/project/misc/Sapporo EastMeetsWest_EP1_GIF_1.gif"
+              alt="sapporo east meets west 2"
+              width={1200}
+              height={800}
               className="h-auto w-full object-cover"
             />
             <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
-              Sapporo East Meets West, Campaign Branding
+              Sapporo East Meets West, Campaign Branding, Social Assets. Campaign <a href="https://tv.booooooom.com/2019/07/26/branded-east-meets-west-koharu-sugawara-matt-vega/" target="_blank" className="underline">Video 🔗</a>
             </p>
           </div>
+
           <div className="mx-auto w-3/5">
             <Image
               src="/project/misc/nouhtrangthao-vice-offsite.gif"
@@ -472,6 +481,7 @@ export default function SelectedProjects() {
             </p>
           </div>
 
+        
           <MediaBlock width="w-1/3">
             <Image
               src="/project/ttr/nouhtrangthao-ttr-carla.gif"
@@ -496,7 +506,82 @@ export default function SelectedProjects() {
               src={`/project/ttr/${img}`}
               alt={`TTR issue image ${i + 1}`}
             />
+            
           ))}
+        
+                        <div className="mx-auto w-1/3">
+            <Image
+              src="/project/ttr/issue 10-presscheck.png"
+              alt="TTR Material Fair Poster"
+              width={1200}
+              height={800}
+              className="h-auto w-full object-cover"
+            />
+                   <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
+               The Third Rail Issue 11
+            </p>
+          </div>
+
+             <div className="mx-auto w-2/3">
+            <Image
+              src="/project/ttr/ttr-materialfair-poster.jpg"
+              alt="TTR Material Fair Poster"
+              width={1200}
+              height={800}
+              className="h-auto w-full object-cover"
+            />
+            <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
+              Material Art Fair Issue 11, Poster
+            </p>
+          </div>
+
+                <div className="mx-auto w-full">
+                          <Image
+              src="/project/vice/ces/vice-ces-0.jpg"
+              alt="ces 1"
+              width={1200}
+              height={800}
+              className="h-auto w-full object-cover mt-2 pb-5 "
+            />
+  <div className="grid grid-cols-2 gap-4">
+    <Image
+      src="/project/vice/ces/vice-ces-1.jpg"
+      alt="ces 2"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    <Image
+      src="/project/vice/ces/vice-ces-2.jpg"
+      alt="ces 3"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    {/* Optional placeholders or additional content */}
+      <Image
+      src="/project/vice/ces/vice-ces-4.jpg"
+      alt="ces 4"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    <Image
+      src="/project/vice/ces/vice-ces-5.jpg"
+      alt="ces 5"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+  </div>
+
+  <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
+    VICE CES Suite, Over 100 print assets, Print Production
+  </p>
+</div>
 
           <MediaBlock>
             <Image
@@ -510,19 +595,20 @@ export default function SelectedProjects() {
               The Third Rail Auction, Identity, Publication
             </p>
           </MediaBlock>
-
-          <div className="mx-auto w-1/3">
+                    <div className="mx-auto w-2/3">
             <Image
-              src="/project/ttr/ttr-materialfair-poster.jpg"
+              src="/project/misc/nouhtrangthao-the_rodina.jpg"
               alt="TTR Material Fair Poster"
               width={1200}
               height={800}
               className="h-auto w-full object-cover"
             />
             <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
-              Material Art Fair Issue 11, Poster
+              MCAD Visiting Artist Lecture Series, Poster
             </p>
           </div>
+
+       
 
           <div className="mx-auto w-1/3">
             <Image
@@ -560,6 +646,46 @@ export default function SelectedProjects() {
               Jerome Foundation Fellowships, Exhibition
             </p>
           </div>
+                <div className="mx-auto w-full">
+  <div className="grid grid-cols-2 gap-4">
+    <Image
+      src="/project/misc/nouhtrangthao-beta-mock.jpg"
+      alt="beta-1"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    <Image
+      src="/project/misc/nouhtrangthao-betaspread.jpg"
+      alt="beta-2"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    {/* Optional placeholders or additional content */}
+      <Image
+      src="/project/misc/nouhtrangthao-betaspread2.jpg"
+      alt="beta-1"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+
+    <Image
+      src="/project/misc/nouhtrangthao-betaspread3.jpg"
+      alt="beta-2"
+      width={1200}
+      height={800}
+      className="h-auto w-full object-cover"
+    />
+  </div>
+
+  <p className="mt-2 pb-5 text-center text-sm text-zinc-400">
+    BETA v.1, Publication
+  </p>
+</div>
         </>
       </motion.section>
     </motion.main>
